a7b6f55ecc4febd75235b029224f3d1b70f3ef22
Ozomozyan <125055262+Ozomozyan@users.noreply.github.com>
2024-03-01 16:19:40 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,41 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #Comm2
-
-@app.route("/contact/")
-def MaPremiereAPI():
-    return render_template('contact.html')
-
-@app.route('/tawarano/')
-def meteo():
-    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
-    raw_content = response.read()
-    json_content = json.loads(raw_content.decode('utf-8'))
-    results = []
-    for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
-        results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results)
-
-@app.route("/rapport/")
-def mongraphique():
-    return render_template("graphique.html")
-
-@app.route("/histogramme/")
-def histographique():
-    return render_template("histogramme.html")
-
-@app.route('/extract-minutes/<date_string>')
-def extract_minutes(date_string):
-        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-        minutes = date_object.minute
-        return jsonify({'minutes': minutes})
-
-@app.route("/commits/")
-def commitsgraphique():
-    return render_template("commits.html")
+    return <h2>Hellow World !</h2>
   
 if __name__ == "__main__":
   app.run(debug=True)
