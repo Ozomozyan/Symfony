c62ef39aa5c9c1a30bd214fbb7d27834c8f52b15
Ozomozyan <125055262+Ozomozyan@users.noreply.github.com>
2024-03-01 16:58:17 +0100

    Create HelloWorldController.php

---
+++ src/Controller/HelloWorldController.php
@@ -0,0 +1,17 @@
+// src/Controller/HelloWorldController.php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloWorldController
+{
+    /**
+     * @Route("/", name="hello_world")
+     */
+    public function index(): Response
+    {
+        return new Response('<h2>Hello World from Symfony 7.0.4!</h2>'); #commit
+    }
+}
