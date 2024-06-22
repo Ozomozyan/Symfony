e4d83e90e12ad6281e479976182964885ace8e09
Ozomozyan <bozuesa@gmail.com>
2024-06-22 22:19:32 +0200

    Initial commit

--- app.py
+++ app.py
@@ -22,5 +22,21 @@ def hello_world():
     conn.close()
     return jsonify(sectors)
 
+@app.route('/sectors')
+def show_sectors():
+    conn = get_db_connection()
+    if conn is not None:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM sectors")
+        sector_data = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        # Creating a list of dictionaries to store column names and data for JSON response
+        columns = [column[0] for column in cursor.description]
+        result = [dict(zip(columns, data)) for data in sector_data]
+        return jsonify(result)
+    else:
+        return jsonify({"error": "Database connection failed"}), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
