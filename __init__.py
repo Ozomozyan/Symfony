c941f5b3854fb2ba803f00ea5e3935a8bd98bbf0
Ozomozyan <bozuesa@gmail.com>
2024-06-25 04:08:13 +0200

    Initial commit

--- __init__.py
+++ __init__.py
@@ -1,31 +1,41 @@
-from flask import Flask, jsonify
-from flask import render_template
+from flask import Flask, jsonify, render_template
 import mysql.connector
 
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = mysql.connector.connect(
-        host='mysql-esat.alwaysdata.net',  # Adjust the host name
-        database='esat_crisis',
-        user='esat',
-        password='C>3Gmt-4_2h3Fp)/'
-    )
-    return conn
+    try:
+        conn = mysql.connector.connect(
+            host='mysql-esat.alwaysdata.net',  # Adjust the host name
+            database='esat_crisis',
+            user='esat',
+            password='C>3Gmt-4_2h3Fp)/'
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print("Error connecting to MySQL: ", err)
+        return None
 
-@app.route('/')                                                                                                                                                        
-def hello_world():                                                                                                                                                     
-    return render_template('hello.html') 
+@app.route('/')
+def home():
+    return render_template('hello.html')
 
-@app.route('/try')
-def hello_world():
+@app.route('/sectors')
+def sectors():
     conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM sectors')
-    sectors = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return jsonify(sectors)
+    if conn is not None:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('SELECT * FROM sectors')
+            sectors_data = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            return jsonify(sectors_data)
+        except mysql.connector.Error as err:
+            print("Error querying MySQL: ", err)
+            return jsonify({"error": "Error querying database"}), 500
+    else:
+        return jsonify({"error": "Database connection failed"}), 500
 
-if __name__ == "__main__":                                                                                                                                             
-  app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
