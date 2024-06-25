4f0da374654bd631de02a93d1f034d9a63c0b5e0
Ozomozyan <bozuesa@gmail.com>
2024-06-25 03:57:51 +0200

    Initial commit

--- __init__.py
+++ __init__.py
@@ -1,15 +1,8 @@
-from flask import Flask, jsonify, render_template
-import mysql.connector
-
-app = Flask(__name__)
-
-
-
-@app.route('/')
-def home():
-    return render_template('hello.html')
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app = Flask(__name__)                                                                                                                                                  
+                                                                                                                                                                       
+@app.route('/')                                                                                                                                                        
+def hello_world():                                                                                                                                                     
+    return render_template('hello.html')                                                                                                                               
+                                                                                                                                                                       
+if __name__ == "__main__":                                                                                                                                             
+  app.run(debug=True)
