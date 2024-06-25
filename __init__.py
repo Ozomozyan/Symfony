f3f2e97593803ac1fe332faa36ecd1f9184e3257
Ozomozyan <bozuesa@gmail.com>
2024-06-25 03:59:15 +0200

    Initial commit

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
-app = Flask(__name__)                                                                                                                                                  
-                                                                                                                                                                       
-@app.route('/')                                                                                                                                                        
-def hello_world():                                                                                                                                                     
-    return render_template('hello.html')                                                                                                                               
-                                                                                                                                                                       
-if __name__ == "__main__":                                                                                                                                             
-  app.run(debug=True)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, this is a test."
+
+if __name__ == "__main__":
+    app.run(debug=True)
