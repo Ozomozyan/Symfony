83570340da84a883091a41ee0228821c4a9bde52
Ozomozyan <bozuesa@gmail.com>
2024-06-25 03:52:19 +0200

    Initial commit

--- __init__.py
+++ __init__.py
@@ -3,39 +3,13 @@ import mysql.connector
 
 app = Flask(__name__)
 
-def get_db_connection():
-    try:
-        conn = mysql.connector.connect(
-            host='mysql-esat.alwaysdata.net',  # Adjust the host name
-            database='esat_crisis',
-            user='esat',
-            password='C>3Gmt-4_2h3Fp)/'
-        )
-        return conn
-    except mysql.connector.Error as err:
-        print("Error connecting to MySQL: ", err)
-        return None
+
 
 @app.route('/')
 def home():
     return render_template('hello.html')
 
-@app.route('/sectors')
-def sectors():
-    conn = get_db_connection()
-    if conn is not None:
-        cursor = conn.cursor()
-        try:
-            cursor.execute('SELECT * FROM sectors')
-            sectors_data = cursor.fetchall()
-            cursor.close()
-            conn.close()
-            return jsonify(sectors_data)
-        except mysql.connector.Error as err:
-            print("Error querying MySQL: ", err)
-            return jsonify({"error": "Error querying database"}), 500
-    else:
-        return jsonify({"error": "Database connection failed"}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
