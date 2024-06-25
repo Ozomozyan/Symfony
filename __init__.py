2be03f3bd821b1b7e2bc6f41f20248f27588affa
Ozomozyan <bozuesa@gmail.com>
2024-06-25 04:05:21 +0200

    Initial commit

--- __init__.py
+++ __init__.py
@@ -1,15 +1,31 @@
-from flask import Flask, jsonify, render_template
-
+from flask import Flask, jsonify
+from flask import render_template
+import mysql.connector
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = mysql.connector.connect(
+        host='mysql-esat.alwaysdata.net',  # Adjust the host name
+        database='esat_crisis',
+        user='esat',
+        password='C>3Gmt-4_2h3Fp)/'
+    )
+    return conn
 
+@app.route('/')                                                                                                                                                        
+def hello_world():                                                                                                                                                     
+    return render_template('hello.html') 
 
-@app.route('/')
-def home():
-    return render_template('hello.html')
+@app.route('/try')
+def hello_world():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM sectors')
+    sectors = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(sectors)
 
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":                                                                                                                                             
+  app.run(debug=True)
