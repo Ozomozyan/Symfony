b54fbd35175c340f1e2323f8fff171fe51e77a94
Ozomozyan <bozuesa@gmail.com>
2024-06-25 04:01:28 +0200

    Initial commit

--- __init__.py
+++ __init__.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def home():
-    return "Hello, this is a test."
+    return render_template('hello.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
